2d9963784d7078d16ae37e65ea743a1893199e7b
Justin Ballard <justinjballard@gmail.com>
2020-01-28 14:17:38 -0800

    bootstrapping

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./App.css";
 import Game from "./Game.js";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/> 
+*/
 function App() {
   return (
     <div className="App">
